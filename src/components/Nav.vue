0f0fc5348e6a933a975d01ddc6e39b93d216a54d
bing-chen <bing-chen@tcl.com>
2019-01-07 10:35:28 +0800

    0.0.1

---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <template v-for="(nav,key) in navList">
+      <el-menu-item v-bind:index="key.toString()">{{nav}}</el-menu-item>
+    </template>
+  </el-menu>
+</template>
+
+<script>
+    export default {
+      name: "Nav",
+      data(){
+        return {
+          navList:['BigData'],
+          activeIndex:'0'
+        }
+      },
+      methods:{
+        handleSelect:function(e){
+          console.log(e);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
